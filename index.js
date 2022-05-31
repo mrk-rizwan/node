819edc8b5c054aa49053f5ba6f407fbdd78fe85d
MRK <rizwan1361535@gmail.com>
2022-05-31 07:02:47 +0500

    31 5 changes

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const dir_path = path.join(__dirname, 'files');
-// for(i=0;i<5;i++){
-//     fs.writeFileSync(`${dir_path}/index${i}.txt`, 'Hello');
-// }
-fs.readdir(dir_path, (err, files)=>{
-    files.forEach(element => {
-        fs.readFile(`${dir_path}/${element}`,'utf8',(err, data)=>{
-            console.log(data);
-        })
-    });
-})
+const app = express();
+
+const files_path = path.join(__dirname, 'files');
+
+app.get('', (req, res)=>{
+    res.send(`Welcome to the homepage, ${req.query.name}`);
+});
+
+    app.use(express.static(files_path));
+app.listen(4500);
